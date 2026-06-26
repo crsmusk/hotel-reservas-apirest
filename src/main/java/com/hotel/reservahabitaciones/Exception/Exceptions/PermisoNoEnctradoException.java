1fbd6ca1eb43ebb5c35c993c0236ac30a431c102
cristian <cristiantrullo41@gmail.com>
2026-06-25 21:00:35 -0500

    refactor(exceptions): remove unused PermisoNoEnctradoException class refactor(dtos): remove empty habitacion DTO class

--- src/main/java/com/hotel/reservahabitaciones/Exception/Exceptions/PermisoNoEnctradoException.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.reservahabitaciones.Exception.Exceptions;
-
-public class PermisoNoEnctradoException extends RuntimeException{
-    public PermisoNoEnctradoException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/hotel/reservahabitaciones/Model/DTOs/salida/habitacion.java
+++
@@ -1,4 +0,0 @@
-package com.hotel.reservahabitaciones.Model.DTOs.salida;
-
-public class habitacion {
-}
